4e1bde7c463c140f7c5ede51915bdf93949c1c10
Dim <cqd2202@yahoo.de>
2019-03-07 08:41:31 +0100

    JFXEX-00026: LineChart example with 3 series

--- de.dc.emf.javafx.xtext.demo.chart/src/de/dc/emf/javafx/xtext/demo/chart/demo/BudgetChartOtherApplication.java
+++ de.dc.emf.javafx.xtext.demo.chart/src/de/dc/emf/javafx/xtext/demo/chart/demo/BudgetChartOtherApplication.java
@@ -21,17 +21,25 @@ public class BudgetChartOtherApplication extends Application{
 	protected Parent getRoot() {
 		BudgetChartOther chart = new BudgetChartOther();
 		List<BudgetChartOther.Data<Number,Number>> exampleData2List = new ArrayList<BudgetChartOther.Data<Number,Number>>();
+		exampleData2List.add(new BudgetChartOther.Data<Number,Number>(0, 5));   
 		exampleData2List.add(new BudgetChartOther.Data<Number,Number>(10, 20));   
 		exampleData2List.add(new BudgetChartOther.Data<Number,Number>(20, 23));   
 		exampleData2List.add(new BudgetChartOther.Data<Number,Number>(25, 25));   
 		exampleData2List.add(new BudgetChartOther.Data<Number,Number>(30, 30));   
 		chart.createSeries("exampleData2", exampleData2List);
 		List<BudgetChartOther.Data<Number,Number>> exampleData10List = new ArrayList<BudgetChartOther.Data<Number,Number>>();
-		exampleData10List.add(new BudgetChartOther.Data<Number,Number>(0, 0));   
-		exampleData10List.add(new BudgetChartOther.Data<Number,Number>(25, 23));   
-		exampleData10List.add(new BudgetChartOther.Data<Number,Number>(40, 25));   
-		exampleData10List.add(new BudgetChartOther.Data<Number,Number>(80, 30));   
+		exampleData10List.add(new BudgetChartOther.Data<Number,Number>(0, 5));   
+		exampleData10List.add(new BudgetChartOther.Data<Number,Number>(25, 5));   
+		exampleData10List.add(new BudgetChartOther.Data<Number,Number>(40, 7));   
+		exampleData10List.add(new BudgetChartOther.Data<Number,Number>(80, 9));   
 		chart.createSeries("exampleData10", exampleData10List);
+		List<BudgetChartOther.Data<Number,Number>> exampleData15List = new ArrayList<BudgetChartOther.Data<Number,Number>>();
+		exampleData15List.add(new BudgetChartOther.Data<Number,Number>(0, 5));   
+		exampleData15List.add(new BudgetChartOther.Data<Number,Number>(5, 10));   
+		exampleData15List.add(new BudgetChartOther.Data<Number,Number>(20, 15));   
+		exampleData15List.add(new BudgetChartOther.Data<Number,Number>(40, 25));   
+		exampleData15List.add(new BudgetChartOther.Data<Number,Number>(80, 29));   
+		chart.createSeries("exampleData15", exampleData15List);
 		return chart;
 	}
 }
