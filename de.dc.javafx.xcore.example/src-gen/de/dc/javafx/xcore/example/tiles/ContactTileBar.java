3aa0de24e344e814b6c1e5c36f36aabf4f568e70
Dim <cqd2202@yahoo.de>
2019-04-16 23:20:48 +0200

    JFXEX-00069: Xtext dsl description #33

--- de.dc.javafx.xcore.example/src-gen/de/dc/javafx/xcore/example/tiles/ContactTileBar.java
+++ de.dc.javafx.xcore.example/src-gen/de/dc/javafx/xcore/example/tiles/ContactTileBar.java
@@ -11,16 +11,16 @@ public class ContactTileBar extends BaseTileBar {
     setHgap(10d);
     setVgap(10d);
     BaseKeyValueTile contactCounter1Tile = new BaseKeyValueTile("ContactCounter1");
-    contactCounter1Tile.setValue("0");
+    contactCounter1Tile.setValue("200");
     add(contactCounter1Tile);
     BaseKeyValueTile contactCounter2Tile = new BaseKeyValueTile("ContactCounter2");
-    contactCounter2Tile.setValue("0");
+    contactCounter2Tile.setValue("200");
     add(contactCounter2Tile);
     BaseKeyValueTile contactCounter3Tile = new BaseKeyValueTile("ContactCounter3");
-    contactCounter3Tile.setValue("0");
+    contactCounter3Tile.setValue("200");
     add(contactCounter3Tile);
     BaseKeyValueTile contactCounter4Tile = new BaseKeyValueTile("ContactCounter4");
-    contactCounter4Tile.setValue("0");
+    contactCounter4Tile.setValue("200");
     add(contactCounter4Tile);
   }
   
