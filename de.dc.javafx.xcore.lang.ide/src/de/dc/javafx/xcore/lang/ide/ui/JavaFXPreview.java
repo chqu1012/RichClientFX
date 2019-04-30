1202275bff902701726eed20da7a76ad840f682e
Dim <cqd2202@yahoo.de>
2019-04-30 15:16:28 +0200

    JFXEX-00109: JavaFX Lang Preview #87

---
+++ de.dc.javafx.xcore.lang.ide/src/de/dc/javafx/xcore/lang/ide/ui/JavaFXPreview.java
@@ -0,0 +1,24 @@
+package de.dc.javafx.xcore.lang.ide.ui;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class JavaFXPreview extends ViewPart {
+
+	public JavaFXPreview() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
