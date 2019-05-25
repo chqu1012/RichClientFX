32d1396bd869ec7419115c6f705c6e1554df19d3
Dim <cqd2202@yahoo.de>
2019-05-25 10:49:54 +0200

    JFXEX-00181: Fixed the ProjectExplorer size, dynamically growing #167

--- de.dc.javafx.xcore.resource.ui/src/de/dc/javafx/xcore/resource/ui/ProjectExplorer.java
+++ de.dc.javafx.xcore.resource.ui/src/de/dc/javafx/xcore/resource/ui/ProjectExplorer.java
@@ -5,12 +5,18 @@ import de.dc.javafx.xcore.resource.Workspace;
 import de.dc.javafx.xcore.resource.ui.view.ProjectExplorerTreeView;
 import javafx.scene.control.Tab;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 public class ProjectExplorer extends Tab {
 
 	public ProjectExplorer(IEmfManager<Workspace> manager) {
 		setText("ProjectExplorer");
 		setGraphic(new ImageView("images/jworkingSet_obj.png"));
-		setContent(new ProjectExplorerTreeView(manager));
+		ProjectExplorerTreeView explorer = new ProjectExplorerTreeView(manager);
+		AnchorPane.setBottomAnchor(explorer, 0d);
+		AnchorPane.setTopAnchor(explorer, 0d);
+		AnchorPane.setLeftAnchor(explorer, 0d);
+		AnchorPane.setRightAnchor(explorer, 0d);
+		setContent(new AnchorPane(explorer));
 	}
 }
