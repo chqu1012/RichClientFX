9041bfa35509fae184bc27519acdbde6c14043a0
Dim <cqd2202@yahoo.de>
2019-05-25 14:39:54 +0200

    JFXEX-00187: Enhanced the ProjectExplorer Example #174

--- de.dc.javafx.xcore.resource.ui/src/de/dc/javafx/xcore/resource/ui/demo/ResourceViewApplication.java
+++ de.dc.javafx.xcore.resource.ui/src/de/dc/javafx/xcore/resource/ui/demo/ResourceViewApplication.java
@@ -2,6 +2,9 @@ package de.dc.javafx.xcore.resource.ui.demo;
 
 import java.io.File;
 
+import de.dc.javafx.xcore.resource.PackageFolder;
+import de.dc.javafx.xcore.resource.Project;
+import de.dc.javafx.xcore.resource.ResourceFactory;
 import de.dc.javafx.xcore.resource.ui.*;
 import de.dc.javafx.xcore.resource.ui.view.*;
 import de.dc.javafx.xcore.di.ApplicationContext;
@@ -13,7 +16,39 @@ public class ResourceViewApplication extends AbstractApplication{
 	@Override
 	public Parent getRoot() {
 		BaseResourceManager manager = new BaseResourceManager();
+		
+		Project javProject = createProject("Java");
+		PackageFolder packageFolder = createPackage("de.dc.java.example");
+		createJavaFile(packageFolder, "HelloWorld1.java");
+		createJavaFile(packageFolder, "HelloWorld2.java");
+		createJavaFile(packageFolder, "HelloWorld3.java");
+		createJavaFile(packageFolder, "HelloWorld4.java");
+		javProject.getResources().add(packageFolder);
+		manager.getRoot().getProjects().add(javProject);
+		
+		manager.getRoot().getProjects().add(createProject("Dot.net"));
+		manager.getRoot().getProjects().add(createProject("Dart"));
+		manager.getRoot().getProjects().add(createProject("Flutter"));
+		manager.getRoot().getProjects().add(createProject("JavaScript"));
 		return new ResourceModelView(manager);
+	}
+
+	private void createJavaFile(PackageFolder packageFolder, String name) {
+		de.dc.javafx.xcore.resource.File javaFile = ResourceFactory.eINSTANCE.createFile();
+		javaFile.setName( name);
+		packageFolder.getResources().add(javaFile);
+	}
+
+	private PackageFolder createPackage(String packageFolderPath) {
+		PackageFolder packageFolder = ResourceFactory.eINSTANCE.createPackageFolder();
+		packageFolder.setName(packageFolderPath);
+		return packageFolder;
+	}
+
+	private Project createProject(String name) {
+		Project project = ResourceFactory.eINSTANCE.createProject();
+		project.setName(name);
+		return project;
 	}
 	
 	public static void main(String[] args) {
