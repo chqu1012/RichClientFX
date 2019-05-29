f6a6ad5658312457faeb1704789d52e766c67227
Dim <cqd2202@yahoo.de>
2019-05-29 23:16:52 +0200

    JFXEX-00206: Showing text in toolbar item rendering if icon not available #202

--- de.dc.javafx.xcore.workbench.ui/src/de/dc/javafx/xcore/workbench/ui/renderer/EmfWorkbenchRenderer.java
+++ de.dc.javafx.xcore.workbench.ui/src/de/dc/javafx/xcore/workbench/ui/renderer/EmfWorkbenchRenderer.java
@@ -4,13 +4,18 @@ import de.dc.javafx.xcore.workbench.ToolbarItem;
 import de.dc.javafx.xcore.workbench.util.WorkbenchSwitch;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 
 public class EmfWorkbenchRenderer extends WorkbenchSwitch<Node>{
 
 	@Override
 	public Node caseToolbarItem(ToolbarItem object) {
+		String text = object.getName()==null?"":object.getName();
 		Button control = new Button();
-		control.setText(object.getName()==null?"":object.getName());
+		if (object.getIcon()==null) {
+			control.setText(text);
+		}
+		control.setTooltip(new Tooltip(text));
 		return control;
 	}
 }
