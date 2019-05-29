099eff2ae39e558520ca471fbcdb5bc9feffee6a
Dim <cqd2202@yahoo.de>
2019-05-29 11:39:51 +0200

    JFXEX-00201: EmfWorkbench Tabs close actions #197

--- de.dc.javafx.xcore.workbench.ui/src/de/dc/javafx/xcore/workbench/ui/EmfWorkbench.java
+++ de.dc.javafx.xcore.workbench.ui/src/de/dc/javafx/xcore/workbench/ui/EmfWorkbench.java
@@ -1,22 +1,55 @@
 package de.dc.javafx.xcore.workbench.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.BorderPane;
 
 public class EmfWorkbench extends AbstractFxmlControl {
 
     @FXML
-    protected void onEditorAreaCloseAllMenuItem(ActionEvent event) {
-    	
-    }
+    protected BorderPane root;
 
     @FXML
-    protected void onEditorAreaCloseMenuItem(ActionEvent event){
-    	
-    }
+    protected TabPane leftTabPane;
 
     @FXML
-    protected void onEditorAreaCloseOthersMenuItem(ActionEvent event){
-    	
-    }
+    protected TabPane editorArea;
+
+    @FXML
+    protected TabPane rightTabPane;
+
+    @FXML
+    protected TabPane bottomTabPane;
+
+    @FXML
+    protected ToolBar toolbar;
+	
+	@FXML
+	protected void onEditorAreaCloseMenuItem(ActionEvent event) {
+		Tab selection = editorArea.getSelectionModel().getSelectedItem();
+		editorArea.getTabs().remove(selection);
+	}
+
+	@FXML
+	protected void onEditorAreaCloseOthersMenuItem(ActionEvent event) {
+		Tab selection = editorArea.getSelectionModel().getSelectedItem();
+		List<Tab> toRemoveTabs = new ArrayList<>();
+		for (Tab tab : editorArea.getTabs()) {
+			if (tab!=selection) {
+				toRemoveTabs.add(tab);
+			}
+		}
+		editorArea.getTabs().removeAll(toRemoveTabs);
+	}
+
+	@FXML
+	protected void onEditorAreaCloseAllMenuItem(ActionEvent event) {
+		editorArea.getTabs().clear();
+	}
 }
