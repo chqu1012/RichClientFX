568bb1dc0ae806edea4189abc902904783aed081
Dim <cqd2202@yahoo.de>
2019-06-05 22:00:56 +0200

    JFXEX-00245: Extract all event class from workbench. ui to de.dc.javafx.xcore.workbench.event #246

---
+++ de.dc.javafx.xcore.workbench.di/src/de/dc/javafx/xcore/workbench/di/EventModule.java
@@ -0,0 +1,17 @@
+package de.dc.javafx.xcore.workbench.di;
+
+import com.google.inject.AbstractModule;
+
+import de.dc.javafx.xcore.workbench.event.EventBroker;
+import de.dc.javafx.xcore.workbench.event.IEventBroker;
+import de.dc.javafx.xcore.workbench.event.ISelectionService;
+import de.dc.javafx.xcore.workbench.event.SelectionService;
+
+public class EventModule extends AbstractModule {
+
+	@Override
+	protected void configure() {
+		bind(IEventBroker.class).to(EventBroker.class).asEagerSingleton();
+		bind(ISelectionService.class).to(SelectionService.class).asEagerSingleton();
+	}
+}
