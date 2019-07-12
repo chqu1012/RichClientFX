dff592b1e1855c3fa5a62ff26affd27d96a1ff8e
Dim <cqd2202@yahoo.de>
2019-07-12 23:06:59 +0200

    JFXEX-00409: Lecture: Render section names #464

--- addon/de.dc.javafx.xcore.workbench.lecture/de.dc.javafx.xcore.workbench.lecture.ui/src/de/dc/javafx/xcore/workbench/lecture/ui/LecturePreviewFX.java
+++ addon/de.dc.javafx.xcore.workbench.lecture/de.dc.javafx.xcore.workbench.lecture.ui/src/de/dc/javafx/xcore/workbench/lecture/ui/LecturePreviewFX.java
@@ -1,28 +1,42 @@
 package de.dc.javafx.xcore.workbench.lecture.ui;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 
+import org.apache.commons.io.FileUtils;
+
+import com.google.common.base.Charsets;
+
 import de.dc.javafx.xcore.workbench.event.EventContext;
+import de.dc.javafx.xcore.workbench.lecture.LectureProject;
+import de.dc.javafx.xcore.workbench.lecture.ui.template.LectureTemplate;
 import de.dc.javafx.xcore.workbench.lecture.ui.view.LecturePreview;
 import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TreeItem;
 import javafx.scene.web.WebView;
 
-public class LecturePreviewFX extends LecturePreview{
+public class LecturePreviewFX extends LecturePreview {
+
+	private LectureTemplate template = new LectureTemplate();
 
 	@Override
 	public void update(EventContext context) {
-		System.out.println("update");
 	}
-	
+
 	@Override
 	public void changed(ObservableValue observable, Object oldValue, Object newValue) {
-		System.out.println("Changed");
-		
-		try {
-			engine.load(getClass().getResource("/reveal.js-3.8.0/demo.html").toURI().toURL().toExternalForm());
-		} catch (MalformedURLException | URISyntaxException e) {
-			e.printStackTrace();
+		TreeItem item = (TreeItem) newValue;
+		if (item.getValue() instanceof LectureProject) {
+			LectureProject p = (LectureProject) item.getValue();
+			String path = getClass().getResource("/reveal.js-3.8.0/demo.html").getFile();
+			try {
+				FileUtils.write(new File(path), template.gen(p), Charsets.UTF_8);
+				engine.load(getClass().getResource("/reveal.js-3.8.0/demo.html").toURI().toURL().toExternalForm());
+			} catch (URISyntaxException | IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
